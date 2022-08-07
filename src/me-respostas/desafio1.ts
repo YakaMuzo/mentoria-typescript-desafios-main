85bb1528f69eb25eea2ba5373f97347e399c817a
YakaMuzo <carlos545466@gmail.com>
2022-08-07 07:29:52 -0300

    mudanças

---
+++ src/me-respostas/desafio1.ts
@@ -0,0 +1,7 @@
+//Desafio 1
+let employee: {code: number, name:string } = {
+    code: 10,
+    name: 'carlos'
+} 
+
+console.log(`O codigo é ${employee.code} e o nome é ${employee.name}`)
---
+++ src/me-respostas/desafio2.ts
@@ -0,0 +1,64 @@
+//Desafio 2
+// Como podemos melhorar o esse código usando TS? 
+/*
+enum Profissao{
+   // Atriz,
+    Padeiro,
+    Cabelereiro,
+    EngenheirO
+}
+
+interface pessoa {
+    nomeI: string;
+    idadeI: number;
+    profissao: Profissao;
+
+}
+
+const pessoa1: pessoa ={
+    nomeI: 'jorge',
+    idadeI:  28,
+    profissao: Profissao.EngenheirO
+}
+
+const pessoa2: pessoa ={
+    nomeI: 'fabio',
+    idadeI: 22,
+    profissao: Profissao.Cabelereiro
+}
+
+const pessoa: {nome: string, idade: number, profissao: Profissao} ={
+    nome:  'sabrina',
+    idade: 27,
+    profissao: Profissao.Porteiro
+}
+
+console.log(pessoa)
+console.log(pessoa1)
+console.log(pessoa2)
+
+
+
+/*
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: Profissao.Atriz
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
+*/
